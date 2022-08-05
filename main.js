ae2f8832a5c9cf8bd22022c734c8f15b726de4a5
yinka <olayinkabayode9@gmail.com>
2022-08-05 13:16:25 +0100

    final

--- main.js
+++ main.js
@@ -9,10 +9,15 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu(){
     if(!showMenu){
         hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
 
         showMenu = true;
     }
     else{
         hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        showMenu = false;
     }
 }
